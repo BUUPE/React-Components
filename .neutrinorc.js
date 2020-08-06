5af1c2b79ecd2a50ea5dd70b0cc3d7004fbea002
Rudhra Raveendran <rudytheninja@gmail.com>
2020-08-06 12:41:36 -0400

    format neutrinorc

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,14 +1,16 @@
 const reactComponents = require('@neutrinojs/react-components');
 
 module.exports = {
-  use: [reactComponents(),
-  (neutrino) => {
-     neutrino.config.output.library("upe-react-components");
-     neutrino.config.output.globalObject("this");
+  use: [
+    reactComponents(),
+    (neutrino) => {
+      neutrino.config.output.library("upe-react-components");
+      neutrino.config.output.globalObject("this");
 
-     // we only want the index for now
-     const index = neutrino.config.entryPoints.get("index");
-     neutrino.config.entryPoints.clear();
-     neutrino.config.entryPoints.set("index", index);
-    }],
+      // we only want the index for now
+      const index = neutrino.config.entryPoints.get("index");
+      neutrino.config.entryPoints.clear();
+      neutrino.config.entryPoints.set("index", index);
+    }
+  ],
 };
